1baf09c704443b1dcee1e613e43a597dd11d4ef3
fernando-bp <fernando.bohorquez@uptc.edu.co>
2024-10-17 12:31:48 -0500

    Primer commit

---
+++ src/test/java/com/example/Product_Order_Management/ProductOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Product_Order_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
